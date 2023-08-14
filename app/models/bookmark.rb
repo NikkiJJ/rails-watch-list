fae6ace24d1947b899a0cfcf0c89906d10b26431
nicola camp <nicola.camp89@yahoo.com>
2023-08-14 10:34:40 +0100

    adding validations

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,2 +1,6 @@
 class Bookmark < ApplicationRecord
+  belongs_to :movie, :list
+  validates :movie, :list, presence: true
+  validates :movie_id, uniqueness: { scope: :list_id, message: 'has already been bookmarked in this list' }
+  validates :comment, length: { minimum: 6 }
 end
