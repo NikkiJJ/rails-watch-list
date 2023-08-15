e55d471dcc2b7db2c4c834312baae37c204c2a54
nicola camp <nicola.camp89@yahoo.com>
2023-08-15 18:03:41 +0100

    added bookmarks controller

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -15,6 +15,7 @@ class ListsController < ApplicationController
 
   def show
     @list = List.find(params[:id])
+    @bookmark = Bookmark.new
   end
 
   def destroy
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
-  get 'lists', to: 'lists#index'
-  get 'lists/new', to: 'lists#new'
-  post 'lists', to: 'lists#create'
-  get 'lists/:id', to: 'lists#show', as: :list
-  delete 'lists/:id', to: 'lists#destroy'
+  resources :lists do
+    resources :bookmarks, only: [:new, :create]
+  end
+  # get 'lists', to: 'lists#index'
+  # get 'lists/new', to: 'lists#new'
+  # post 'lists', to: 'lists#create'
+  # get 'lists/:id', to: 'lists#show', as: :list
+  # delete 'lists/:id', to: 'lists#destroy'
+
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
