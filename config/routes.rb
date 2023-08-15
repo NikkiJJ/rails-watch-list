838e5e995e5359c4bb4b144edd403aeae6ae351f
nicola camp <nicola.camp89@yahoo.com>
2023-08-15 11:55:45 +0100

    adding lists routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  resources :movies
-  resources :bookmarks
-  resources :lists
+  get 'lists', to: 'lists#index'
+  get 'lists/new', to: 'listss#new'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
