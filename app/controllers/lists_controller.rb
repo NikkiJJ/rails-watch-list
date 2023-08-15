8d6548ce3b679ebffdba2458e43111e9dc1fc0b2
nicola camp <nicola.camp89@yahoo.com>
2023-08-15 12:46:47 +0100

    list controller completed

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -6,4 +6,27 @@ class ListsController < ApplicationController
   def new
     @list = List.new
   end
+
+  def create
+    @list = List.new(list_params)
+    @list.save
+    redirect_to lists_path(@list)
+  end
+
+  def show
+    @list = List.find(params[:id])
+  end
+
+  def destroy
+    @list = List.find(params[:id])
+    @list.destroy
+    redirect_to lists_path, status: :see_other
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:name)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   get 'lists', to: 'lists#index'
-  get 'lists/new', to: 'listss#new'
+  get 'lists/new', to: 'lists#new'
+  post 'lists', to: 'lists#create'
+  get 'lists/:id', to: 'lists#show', as: :list
+  delete 'lists/:id', to: 'lists#destroy'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
